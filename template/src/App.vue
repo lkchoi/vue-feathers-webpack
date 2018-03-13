ab493d420f5d0f78e72a448f2b32d89946e268e1
lkchoi <lester.k.choi@gmail.com>
2018-03-13 00:42:44 -0400

    vue-feathers webpack starter template

---
+++ template/src/App.vue
@@ -0,0 +1,54 @@
+{{#pug}}
+<template lang="pug">
+  #app
+    img(src="./assets/logo.png")
+    {{#router}}
+    router-view
+    {{else}}
+    home
+    {{/router}}
+</template>
+{{else}}
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    {{#router}}
+    <router-view/>
+    {{else}}
+    <Home/>
+    {{/router}}
+  </div>
+</template>
+{{/pug}}
+
+<script>
+{{#unless router}}
+import Home from './pages/home'
+
+{{/unless}}
+export default {
+  name: 'App'{{#router}}{{else}},
+  components: {
+    Home
+  }{{/router}}
+}
+</script>
+
+{{#sass}}
+<style lang="sass">
+  #app
+    font-family: 'Avenir', Helvetica, Arial, sans-serif
+    text-align: center
+    color: #2c3e50
+    margin-top: 60px
+</style>
+{{else}}
+<style>
+  #app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+  }
+</style>
+{{/sass}}
